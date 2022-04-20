99f0a4e3f7e3a89bc187328beed2c9fdab77d8b2
Funn Nghee <nghi.nguyen1805vt@hcmut.edu.vn>
2022-04-20 12:02:09 +0700

    os Getenv for getting PORT

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"go-facebook-bot/pkg/fb"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/webhook", fb.HandleMessenger)
+	http.HandleFunc("/", homepageHandler)
 
-	port := ":4000"
-	log.Fatal(http.ListenAndServe(port, nil))
+	// port := ":4000"
+	fmt.Println(os.Getenv("PORT"))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
+
+func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Home page here !!"))
 }
