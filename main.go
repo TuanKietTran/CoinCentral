fddb12aa6279172d9bb034e56d1d1e75c5cff514
Funn Nghee <nghi.nguyen1805vt@hcmut.edu.vn>
2022-04-25 14:11:06 +0700

    after setup ENV VARS in HEROKU

--- main.go
+++ main.go
@@ -5,27 +5,24 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"regexp"
-
-	"github.com/joho/godotenv"
 )
 
 const projectDirName = "go-facebook-bot"
 
 func getEnv() {
-	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
-	currentWorkDirectory, _ := os.Getwd()
-	rootPath := projectName.Find([]byte(currentWorkDirectory))
+	// projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	// currentWorkDirectory, _ := os.Getwd()
+	// rootPath := projectName.Find([]byte(currentWorkDirectory))
 
-	err := godotenv.Load(string(rootPath) + `/.env`)
+	// err := godotenv.Load(string(rootPath) + `/.env`)
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 }
 
 func main() {
-	getEnv()
+	// getEnv()
 
 	http.HandleFunc("/webhook", fb.HandleMessenger)
 	http.HandleFunc("/", homepageHandler)
